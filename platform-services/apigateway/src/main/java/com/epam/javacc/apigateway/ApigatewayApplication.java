13fb3789507f8397f6f27160353427b4dbc0df31
Bobur_Zokirov <zokirovbobur93@gmail.com>
2021-11-14 01:01:00 +0500

    working registry and gateway

---
+++ platform-services/apigateway/src/main/java/com/epam/javacc/apigateway/ApigatewayApplication.java
@@ -0,0 +1,17 @@
+package com.epam.javacc.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableEurekaClient
+@EnableZuulProxy
+@SpringBootApplication
+public class ApigatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigatewayApplication.class, args);
+	}
+
+}
